5d99f6e96e556fb589ef89ac7e9e773081d99a35
Arthur Guazzelli <arthur.b.guazzelli@gmail.com>
2026-04-01 18:49:04 -0300

    chore: version bump

--- VehicleDoorsOverhauled/VehicleDoorsOverhauled.cs
+++ VehicleDoorsOverhauled/VehicleDoorsOverhauled.cs
@@ -8,7 +8,7 @@ namespace VehicleDoorsOverhauled
     public override string ID => "VehicleDoorsOverhauled"; // Your (unique) mod ID 
     public override string Name => "Vehicle Doors Overhauled"; // Your mod name
     public override string Author => "casper-3"; // Name of the Author (your name)
-    public override string Version => "1.2.0"; // Version
+    public override string Version => "1.3.0"; // Version
     public override string Description => "Overhauls vehicle door controls to use left and right mouse buttons."; // Short description of your mod
     public override Game SupportedGames => Game.MyWinterCar | Game.MySummerCar; //Supported Games
 
